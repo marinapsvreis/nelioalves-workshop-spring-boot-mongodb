78eefaa94b91cd8161bba1c009bd732ffd572b77
Marina Portugal <marinapsvreis@gmail.com>
2022-06-05 20:13:09 -0300

    Obtendo um usuario por ID

--- src/main/java/com/nelioalves/workshopmongo/services/UserService.java
+++ src/main/java/com/nelioalves/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.nelioalves.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nelioalves.workshopmongo.domain.User;
 import com.nelioalves.workshopmongo.repository.UserRespository;
+import com.nelioalves.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return userRespository.findAll();
+	}
+	
+	public User findById(String id){
+		Optional<User> obj = userRespository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
 }
