bf2b5e285817f7cb26c6faeca50a1259a72f1c85
Marina Portugal <marinapsvreis@gmail.com>
2022-06-05 21:26:34 -0300

    Endpoint para retornar os posts de um usuario

--- src/main/java/com/nelioalves/workshopmongo/config/Instantiation.java
+++ src/main/java/com/nelioalves/workshopmongo/config/Instantiation.java
@@ -38,7 +38,7 @@ public class Instantiation implements CommandLineRunner {
 		
 		userRespository.saveAll(Arrays.asList(maria, alex, bob));
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajara para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
 		postRespository.saveAll(Arrays.asList(post1, post2));
